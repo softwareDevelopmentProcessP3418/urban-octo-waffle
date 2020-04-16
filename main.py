8306358f1175402241ead61118e1a7e0f9e61dd5
Gleb Suhanov <suhanovgleb@inbox.ru>
2020-04-16 18:44:28 +0500

    input file bad symbol detection and tokenizing (#11)
    * input file bad symbol detection and tokenizing
    
    * exception and append refactoring
    
    * import, codestyle and other refactoring
    
    * SNAKE_STYLE refactor

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 from sys import argv
+from enum import Enum
 
 l = [0] * 30000
 ptr = 0    
 
-def handle(char):
-    print(char)
+class BrainfuckSymbols(Enum):
+    GREATER_THAN = '>'
+    LESS_THAN = '<'
+    PLUS = '+'
+    MINUS = '-'
+    DOT = '.'
+    COMMA = ','
+    OPEN_BRACKET = '['
+    CLOSE_BRACKET = ']'
+
+    @staticmethod
+    def get_token(char):
+        switcher = {
+            BrainfuckSymbols.GREATER_THAN.value: BrainfuckSymbols.GREATER_THAN,
+            BrainfuckSymbols.LESS_THAN.value: BrainfuckSymbols.LESS_THAN,
+            BrainfuckSymbols.PLUS.value: BrainfuckSymbols.PLUS,
+            BrainfuckSymbols.MINUS.value: BrainfuckSymbols.MINUS,
+            BrainfuckSymbols.DOT.value: BrainfuckSymbols.DOT,
+            BrainfuckSymbols.COMMA.value: BrainfuckSymbols.COMMA,
+            BrainfuckSymbols.OPEN_BRACKET.value: BrainfuckSymbols.OPEN_BRACKET,
+            BrainfuckSymbols.CLOSE_BRACKET.value: BrainfuckSymbols.CLOSE_BRACKET
+        }
+        token = switcher.get(char, None)
+        if token is None:
+            raise IOError('Error: Symbol \'' + char + '\' is not the part of a Brainfuck language')
+        return token
 
 def main():
     filename = argv[1]
+    tokenized_symbols = []
     with open(filename) as f:
         for line in f:
             for c in line:
-                handle(c)
+                token = BrainfuckSymbols.get_token(c)
+                tokenized_symbols.append(token)
 
 
 if __name__ == '__main__':
