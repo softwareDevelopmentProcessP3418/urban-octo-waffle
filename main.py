bc33bbed3c6b0b9f8ddbdb3fed66da5d831fc364
Gleb Suhanov <suhanovgleb@inbox.ru>
2020-04-26 01:29:44 +0500

    Enum refactoring (#21)
    * increment and decrement current cell
    
    * increment and decrement current cell implementation
    
    * increment and decrement current cell implementation
    
    * add missing reset
    
    * massive refactoring
    
    * merge with master
    
    * now really merge with master

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from sys import argv
-from enum import Enum
 
 
 class State:
@@ -12,7 +11,7 @@ class State:
 
     def move_ptr_right(self):
         if (self.ptr == (len(self.array) - 1)):
-            self.array.append(0)        
+            self.array.append(0)
         self.ptr += 1
 
     def move_ptr_left(self):
@@ -28,45 +27,31 @@ class State:
         self.array[self.ptr] -= 1
 
 
-class BrainfuckSymbols(Enum):
-    GREATER_THAN = '>'
-    LESS_THAN = '<'
-    PLUS = '+'
-    MINUS = '-'
-    DOT = '.'
-    COMMA = ','
-    OPEN_BRACKET = '['
-    CLOSE_BRACKET = ']'
-    EXCLAMATION_MARK = '!'
-
-    @staticmethod
-    def get_token(char):
-        switcher = {
-            BrainfuckSymbols.GREATER_THAN.value: BrainfuckSymbols.GREATER_THAN,
-            BrainfuckSymbols.LESS_THAN.value: BrainfuckSymbols.LESS_THAN,
-            BrainfuckSymbols.PLUS.value: BrainfuckSymbols.PLUS,
-            BrainfuckSymbols.MINUS.value: BrainfuckSymbols.MINUS,
-            BrainfuckSymbols.DOT.value: BrainfuckSymbols.DOT,
-            BrainfuckSymbols.COMMA.value: BrainfuckSymbols.COMMA,
-            BrainfuckSymbols.OPEN_BRACKET.value: BrainfuckSymbols.OPEN_BRACKET,
-            BrainfuckSymbols.CLOSE_BRACKET.value: BrainfuckSymbols.CLOSE_BRACKET,
-            BrainfuckSymbols.EXCLAMATION_MARK.value: BrainfuckSymbols.EXCLAMATION_MARK
-        }
-        token = switcher.get(char, None)
-        if token is None:
-            raise IOError('Error: Symbol \'' + char + '\' is not the part of a Brainfuck language')
-        return token
+def get_action(char):
+    switcher = {
+        '>': State.move_ptr_right,
+        '<': State.move_ptr_left,
+        '+': State.increment_current_cell,
+        '-': State.decrement_current_cell,
+        '.': None,
+        ',': None,
+        '[': None,
+        ']': None,
+        '!': State.reset
+    }
+    action = switcher.get(char, None)
+    if action is None:
+        raise IOError('Symbol \'' + char + '\' is not the part of a Brainfuck language')
+    return action
 
 
 def main():
     filename = argv[1]
-    tokenized_symbols = []
     with open(filename) as f:
         for line in f:
             for c in line:
-                token = BrainfuckSymbols.get_token(c)
-                tokenized_symbols.append(token)
-    
+                pass
+
 
 if __name__ == '__main__':
     main()
